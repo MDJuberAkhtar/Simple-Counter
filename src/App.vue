9425baf249d763b2591564b37b42aa7efc29ff3a
MD Juber Akhtar <vastulorde99@gmail.com>
2020-10-20 10:51:04 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Simple Vue Counter" :counter="counter"/>
   </div>
 </template>
 
@@ -12,6 +11,11 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      counter:0
+    }
   }
 }
 </script>
@@ -23,6 +27,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
